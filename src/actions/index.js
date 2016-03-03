ff5e79d9a8d4d7dd82efb04745011bc796e98a00
darwin <oleg.umarov@gmail.com>
2016-03-03 15:45:14 -0600

    clean up

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,56 +9,10 @@ export function addTodo(text) {
     text
   };
 }
-
-export function toggleTodo(_id) {
-  return {
-    type: ACTIONS.TOGGLE_TODO,
-    _id
-  };
-}
-
 export function fetchTodos() {
   const request = axios.get(`api/task`);
   return {
     type: ACTIONS.FETCH_TODOS,
     payload: request
-  };
-}
-
-export function postTodo(todo) {
-  const request = axios.post(`api/task/`, todo);
-  return {
-    type: ACTIONS.POST_TODO,
-    payload: request
-  };
-}
-
-export function deleteTodo(id) {
-  const request = axios.delete(`api/task/${id}`);
-  return {
-    type: ACTIONS.DELETE_TODO,
-    payload: request,
-    id
-  };
-}
-
-export function showAll(todos) {
-  return {
-    type: ACTIONS.SHOW_ALL,
-    payload: todos
-  };
-}
-
-export function showCompleted(todos) {
-  return {
-    type: ACTIONS.SHOW_COMPLETED,
-    payload: todos
-  };
-}
-
-export function showInProgress(todos) {
-  return {
-    type: ACTIONS.SHOW_IN_PROGRESS,
-    payload: todos
   };
 }
