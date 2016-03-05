3a1f7dfe01d113bb4d1b5a0a7378232e96447a21
darwin <oleg.umarov@gmail.com>
2016-03-04 19:20:50 -0600

    refactor navbar

---
+++ src/containers/navbar/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Colors from 'material-ui/lib/styles/colors';
+
+const style = {
+  logo: {
+    color: Colors.white,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+  link: {
+    fontWeight: 'bold',
+    fontSize: 18
+  }
+};
+
+export default class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="blue darken-2">
+        <div className="nav-wrapper" style={style.navbar}>
+          <a href="#!" className="brand-logo" style={style.logo}>PTC Portal</a>
+          <a href="#" style={style.logo} data-activates="mobile-demo" className="button-collapse">
+            <i className="fa fa-align-justify"></i>
+          </a>
+          <ul className="right hide-on-med-and-down" style={style.link}>
+            <li><a href="" style={style.link}>Add Student</a></li>
+            <li><a href="" style={style.link}>Results</a></li>
+            <li><a href="" style={style.link}>Dashboard</a></li>
+            <li><a href="" style={style.link}>Guide</a></li>
+            <li><a href="" style={style.link}>Login</a></li>
+          </ul>
+          <ul className="side-nav" id="mobile-demo" style={style.link}>
+            <li><a href="">Add Student</a></li>
+            <li><a href="">Results</a></li>
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Guide</a></li>
+            <li><a href="">Login</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
