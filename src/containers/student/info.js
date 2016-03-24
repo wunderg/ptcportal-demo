3f9ac8bea0252bacc7d505469cab90dd9c7d3ebd
darwin <oleg.umarov@gmail.com>
2016-03-23 23:27:14 -0500

    break student page into components

---
+++ src/containers/student/info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default (props) => (
+    <div className="card blue-grey darken-1">
+      <div className="card-content white-text">
+        <span className="card-title">Info</span>
+        <section>Contacted</section>
+        <section>Lesson</section>
+        <section>Interview date</section>
+        <section>Decision</section>
+      </div>
+    </div>
+);
