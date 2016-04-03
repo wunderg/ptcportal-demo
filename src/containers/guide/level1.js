da99e62e7e0a9124dc71c1daf6a25740cca9a612
darwin <oleg.umarov@gmail.com>
2016-04-03 18:45:02 -0500

    create lesson layout

--- src/containers/guide/level1.js
+++ src/containers/guide/level1.js
@@ -5,10 +5,13 @@ const each = `function each(collection, callback){
   //your code here
 }`
 
+const map = `function map(collection, callback) {
+  //your code here
+}`
 
 export default () => (
 
-  <div className="card grey lighten-2">
+  <div className="card deep-purple lighten-5">
     <div className="card-content">
       <h3>Level 1</h3>
       <hr />
@@ -18,14 +21,29 @@ export default () => (
             <div className="card-content">
               <h4><b>Lesson 1</b></h4>
               <hr />
+              <div className="">
+                <h5><b>Objectives:</b></h5>
+                <ul className="prompts">
+                  <li><i className="fa fa-check"></i> Create function each</li>
+                  <li><i className="fa fa-check"></i> Each should accept collection and callback as parameters</li>
+                  <li><i className="fa fa-check"></i> Each should work on arrays and objects</li>
+                </ul>
+              </div>
+              <div className="">
+                <h5><b>Technical:</b></h5>
+              </div>
               <ul className="prompts">
-               <li>Create function each</li> 
-               <li>Each should accept collection and callback as parameters</li>
-               <li>Each should work on arrays and objects</li>
+                <li><i className="fa fa-check"></i> Create function each</li>
+                <li><i className="fa fa-check"></i> Each should accept collection and callback as parameters</li>
+                <li><i className="fa fa-check"></i> Each should work on arrays and objects</li>
               </ul>
             </div>
             <div className="code">
               <h5><b>Boilerplate:</b></h5>
+              <Highlight props={each} />
+            </div>
+            <div className="code">
+              <h5><b>Solution:</b></h5>
               <Highlight props={each} />
             </div>
           </div>
@@ -33,14 +51,22 @@ export default () => (
         <li>
           <div className="card">
             <div className="card-content">
-              Create each function, that accepts collection and callback, each should be able to handle arrays and objects
+              <h4><b>Lesson 2</b></h4>
+              <hr />
+              <div className="">
+                <h5><b>Objectives:</b></h5>
+              </div>
+              <ul className="prompts">
+                <li><i className="fa fa-check"></i> Create function map</li>
+                <li><i className="fa fa-check"></i> Map should apply callback to every item in the collection</li>
+                <li><i className="fa fa-check"></i> Map should accept collection and callback as parameters</li>
+                <li><i className="fa fa-check"></i> Map should work on arrays and objects</li>
+                <li><i className="fa fa-check"></i> Map should return a new array</li>
+              </ul>
             </div>
-          </div>
-        </li>
-        <li>
-          <div className="card">
-            <div className="card-content">
-              Create each function, that accepts collection and callback, each should be able to handle arrays and objects
+            <div className="code">
+              <h5><b>Boilerplate:</b></h5>
+              <Highlight props={map} />
             </div>
           </div>
         </li>
