b2307d444d4256d8b3197e2329d38cfae1e9845b
darwin <oleg.umarov@gmail.com>
2016-04-03 22:46:56 -0500

    complete lesson 1 level 1

--- src/containers/guide/level1.js
+++ src/containers/guide/level1.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import Highlight from '../../helpers/highlight.js';
 
-const each = `function each(collection, callback){
+const each =
+`function each(collection, callback){
   //your code here
 }`
+
+const eachSolution =
+`function each(collection, callback) {
+  if (Array.isArray(collection)) {
+    for (var i = 0; i < collection.length ; i++) {
+      callback(collection[i]);
+    }
+  } else {
+    for (var prop in collection) {
+      callback(collection[prop]);
+    }
+  }
+}
+
+***** Pseudo Code *****
+//declare function each
+  //check if collection is array
+    //iterate over array
+      //apply callback on every item in the array
+  // else if collection is not an array
+    // iterate over object
+      //apply callback to every value in the object
+`
+
+const eachHelpers =
+`//test array
+var numbers = [1,2,3,4,5,6];
+
+//test function
+function log(num) {
+  console.log(num);
+}
+`
 
 const map = `function map(collection, callback) {
   //your code here
@@ -24,9 +58,10 @@ export default () => (
               <div className="">
                 <h5><b>Objectives:</b></h5>
                 <ul className="prompts">
-                  <li><i className="fa fa-check"></i><span> Create function each</span></li>
-                  <li><i className="fa fa-check"></i><span> Create function each</span></li>
-                  <li><i className="fa fa-check"></i><span> Create function each</span></li>
+                  <li><i className="fa fa-check"></i><span> Understand how to distinguish between an array and/or an object</span></li>
+                  <li><i className="fa fa-check"></i><span> Understand how to iterate over an array and/or an object </span></li>
+                  <li><i className="fa fa-check"></i><span> Understand how to access item inside an array and/or object </span></li>
+                  <li><i className="fa fa-check"></i><span> Understand how to apply callback to every item in an array and/or object</span></li>
                 </ul>
               </div>
               <div className="">
@@ -36,6 +71,7 @@ export default () => (
                 <li><i className="fa fa-check"></i><span> Create function each</span></li>
                 <li><i className="fa fa-check"></i><span> Each should accept collection and callback as parameters</span></li>
                 <li><i className="fa fa-check"></i><span> Each should work on arrays and objects</span></li>
+                <li><i className="fa fa-check"></i><span> Each should apply the callback to every item in the collection</span></li>
               </ul>
             </div>
             <div className="code">
@@ -44,11 +80,11 @@ export default () => (
             </div>
             <div className="code">
               <h5><b>Solution:</b></h5>
-              <Highlight props={each} />
+              <Highlight props={eachSolution} />
             </div>
             <div className="code">
               <h5><b>Helpers:</b></h5>
-              <Highlight props={each} />
+              <Highlight props={eachHelpers} />
             </div>
           </div>
         </li>
