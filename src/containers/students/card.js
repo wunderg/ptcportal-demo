8e2fd14a570c2eae19f0066e13d8af6a783699e1
darwin <oleg.umarov@gmail.com>
2016-04-05 23:51:38 -0500

    create basic layout for student cards

--- src/containers/students/card.js
+++ src/containers/students/card.js
@@ -14,14 +14,15 @@ export default ({ student }) => (
           <span className="card-title activator grey-text text-darken-4" >{student.name}<i className="fa fa-info right"></i></span>
           <hr />
           <span>{student.email}</span>
+          <p className='contacted'> Contacted: <i className="green-text text-darken-1 fa fa-check right checkmark"></i> </p>
           <span className="progress-text">Current progress: 5 of 8</span>
           <Progress className="students-progress-bar" />
-          <a className="waves-effect waves-light btn right" onClick={onClick}>Details</a>
+            <a className="waves-effect waves-light btn right details" onClick={onClick}>Details</a>
         </div>
         <div className="card-reveal">
-          <span className="card-title grey-text text-darken-4">Card Title<i className="fa fa-close right"></i></span>
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p className='contacted'> Contacted: <i className="fa fa-check right"></i> </p>
+          <span className="card-title purple-text text-darken-5">Actions<i className="fa fa-close right"></i></span>
+          <p className='contacted'> Contacted: <i className="fa fa-check right checkmark"></i> </p>
+          <p className='contacted'> Contacted: <i className="fa fa-check right checkmark"></i> </p>
         </div>
       </div>
     </div>
