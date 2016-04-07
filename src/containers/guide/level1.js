57749c9aec05f88ac800ffdcdb93c10bf73c9780
darwin <oleg.umarov@gmail.com>
2016-04-06 21:06:13 -0500

    move each to separate file for an easy editing

--- src/containers/guide/level1.js
+++ src/containers/guide/level1.js
@@ -1,45 +1,6 @@
 import React from 'react';
 import Highlight from '../../helpers/highlight.js';
-
-const each =
-`function each(collection, callback){
-  //your code here
-}`
-
-const eachSolution =
-`function each(collection, callback) {
-  if (Array.isArray(collection)) {
-    for (var i = 0; i < collection.length ; i++) {
-      callback(collection[i]);
-    }
-  } else {
-    for (var prop in collection) {
-      callback(collection[prop]);
-    }
-  }
-}
-
-***** Pseudo Code *****
-// declare function each
-  // check if collection is array
-    // iterate over array
-      // apply callback on every item in the array
-  // else if collection is not an array
-    // iterate over object
-      // apply callback to every value in the object
-`
-
-const eachExample =
-`//test array
-var numbers = [1,2,3,4,5,6];
-
-//test function
-function log(num) {
-  console.log(num);
-}
-
-each(numbers, log); => log every value to the console;
-`
+import { each, eachExample, eachSolution} from './code/each.js';
 
 const map = `function map(collection, callback) {
   //your code here
