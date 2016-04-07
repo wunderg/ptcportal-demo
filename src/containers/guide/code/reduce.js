6b34d6f598342585a906ec655d49c6051fb83116
darwin <oleg.umarov@gmail.com>
2016-04-07 00:11:18 -0500

    add code snippets for reduce

---
+++ src/containers/guide/code/reduce.js
@@ -0,0 +1,41 @@
+export const reduce = `function reduce(collection, accumulator, startValue) {
+  //your code here
+}`
+
+export const reduceSolution =
+`function reduce(collection, accumulator, startValue) {
+  each(collection, function(item) {
+    if (startValue === undefined) {
+      startValue = collection[0];
+    }
+    else {
+      startValue = accumulator(startValue, item);
+    }
+  });
+
+  return startValue;
+}
+***** Pseudo Code *****
+// declare function reduce that accept collection, accumulator & start
+
+  // use each to iterate over collection
+  // and pass anonymous funcion as callback
+     // if start is not defined
+        // then start will be first item in the collection
+     // else
+        // start will be the result of accumulator with start and item
+
+//return start
+`
+
+export const reduceExample =
+`//test array
+var numbers = [1,2,3,4];
+
+//test function
+function sum(a + b) {
+  return a + b;
+}
+
+reduce(numbers, double); => 10;
+`
