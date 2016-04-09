f818bbfc6f3e3c4d22016cb0651fd972cb023da9
darwin <oleg.umarov@gmail.com>
2016-04-08 22:05:15 -0500

    add getMax boilerplate and example

--- src/containers/guide/code/getMax.js
+++ src/containers/guide/code/getMax.js
@@ -1,6 +1,20 @@
 export const getMax =
 `
 function getMax(collection) {
+  // code here
+}
+`;
+
+export const getMaxExample =
+`
+var numbers = [3,6,9,8,6,3,9];;
+
+getMax(numbers) => 9;
+`;
+
+export const getMaxSolution =
+`
+function getMax(collection) {
   return reduce(collection, function(memo, item) {
     if (item > memo) {
       memo = item;
