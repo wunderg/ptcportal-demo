b65896b3ba2611fda2cfa6799e61df39c8551320
darwin <oleg.umarov@gmail.com>
2016-04-10 17:25:40 -0500

    change the format a little

--- src/containers/guide/howto.js
+++ src/containers/guide/howto.js
@@ -13,21 +13,66 @@ export default () => (
               <hr />
               <div className="">
                 <ul className="prompts">
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Student have to complete every exercise</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Student have to write every function from scratch</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Student have to start at Level 1</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Student have to write every function that will be used in the lesson</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Student have to talk thru every solution on a high level as well as line by line</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Every session will start with Level 1.</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> If level had been completed previously only one lesson could be choosen</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Before the Mock Interview student have to finish level 1-7 within ONE session</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Never copy and paste code from this guide to student</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Student have to write every function line by line - NO CHEATING</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Use Pseudo-code to show student on how you would talk about the function</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Have them explain every function in details</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> If student comfortable to write function without example is OK</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Especially after first few lessons, when he is comfortable with each and reduce</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> For the best results always have student to refer to his own implementations</span></li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Student have to complete every exercise</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Student have to write every function from scratch</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Student have to start at Level 1</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Student have to write every function that will be used in the lesson</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Student have to talk thru every solution on a high level as well as line by line</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Every session will start with Level 1.</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> If level had been completed previously only one lesson could be choosen</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Before the Mock Interview student have to finish level 1-7 within ONE session</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Never copy and paste code from this guide to student</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Student have to write every function line by line - NO CHEATING</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Use Pseudo-code to show student on how you would talk about the function</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Have them explain every function in details</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> If student comfortable to write function without example it is OK</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Especially after first few lessons, when he is comfortable with each and reduce</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> For the best results always have student to refer to his own implementations</span>
+                  </li>
 
                 </ul>
               </div>
@@ -42,10 +87,21 @@ export default () => (
               <hr />
               <div className="">
                 <ul className="prompts">
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Never give up the ANSWER to the student</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Be kind and courteous. Being mean or rude scares people off and makes people not want to participate.</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Low-effort criticism is for the benefit of the person delivering it, not the person receiving it.</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> No feigning surprise. Don't act surprised when somebody says they don't know something. They'll feel terrible and you've accomplished nothing except to make yourself feel sharp.</span></li>
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Never give up the ANSWER to the student</span>
+                  </li>
+
+                  <li><i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Be kind and courteous. Being mean or rude scares people off and makes people not want to participate.</span></li>
+
+                  <li><i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> Low-effort criticism is for the benefit of the person delivering it, not the person receiving it.</span></li>
+
+                  <li>
+                    <i className="fa fa-check green-text text-lighten-2"></i>
+                    <span> No feigning surprise. Don't act surprised when somebody says they don't know something. They'll feel terrible and you've accomplished nothing except to make yourself feel sharp.</span>
+                  </li>
                   <li><i className="fa fa-check green-text text-lighten-2"></i><span> No well-actually's. When someone says something that's almost - but not entirely - correct, and you say, "well, actually…" and then give a minor correction. This is especially annoying when the correction has no bearing on the actual conversation. </span></li>
                   <li><i className="fa fa-check green-text text-lighten-2"></i><span> Walking thru every iteration is the best way learn</span></li>
                   <li><i className="fa fa-check green-text text-lighten-2"></i><span> Help student to develop 'Code Compiler' in his head</span></li>
