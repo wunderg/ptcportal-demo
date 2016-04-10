b9f68851363c00bfee19c70ed9cb3026017e90cc
darwin <oleg.umarov@gmail.com>
2016-04-09 23:02:30 -0500

    add howto page

---
+++ src/containers/guide/howto.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+export default () => (
+  <div className="card deep-purple lighten-5">
+    <div className="card-content">
+      <h3>How To Use This Guide</h3>
+      <hr />
+      <ul className="lesson-list">
+        <li>
+          <div className="card">
+            <div className="card-content">
+              <h4><b>Workflow</b></h4>
+              <hr />
+              <div className="">
+                <ul className="prompts">
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Student have to complete every exercise</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Student have to write every function from scratch</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Student have to start at Level 1</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Student have to write every function that will be used in the lesson</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Student have to talk thru every solution on a high level as well as line by line</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Every session will start with Level 1.</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> If level had been completed previously only one lesson could be choosen</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Before the Mock Interview student have to finish level 1-7 within ONE session</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Never copy and paste code from this guide to student</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Student have to write every function line by line - NO CHEATING</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Use Pseudo-code to show student on how you would talk about the function</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Have them explain every function in details</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> If student comfortable to write function without example is OK</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Especially after first few lessons, when he is comfortable with each and reduce</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> For the best results always have student to refer to his own implementations</span></li>
+
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+
+        <li>
+          <div className="card">
+            <div className="card-content">
+              <h4><b>Tips</b></h4>
+              <hr />
+              <div className="">
+                <ul className="prompts">
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Never give up the ANSWER to the student</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Walking thru every iteration is the best way learn</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Help student to develop 'Code Compiler' in his head</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Let student to write his solution before the suggestions</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Lead a student to correct answer with suggestion and small hints</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Be encouraging, polite and supportive thru every step</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Make sure to PRAISE STUDENT on every lesson completed</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Even simple 'Good Job' is sufficent</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Show student how to use chrome debbuger to walk thru every iteration</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
