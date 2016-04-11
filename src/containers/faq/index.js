ed95ca98a506ae70ae6a2ec6496a9653eef5524d
darwin <oleg.umarov@gmail.com>
2016-04-10 22:19:45 -0500

    complete initial layout for faq and res

--- src/containers/faq/index.js
+++ src/containers/faq/index.js
@@ -5,15 +5,12 @@ class Faq extends Component {
 
   render() {
     return (
-      <div className="row">
-        <div className="col s12 m9 l10">
-          <div className="card deep-purple lighten-5">
-            <div className="card-content">
-              <h3>Frequently Asked Questions</h3>
-              <hr />
-            </div>
+      <div className="row faq">
+        <div className="card deep-purple lighten-5">
+          <div className="card-content">
+            <h3>Frequently Asked Questions</h3>
+            <hr />
           </div>
-
         </div>
       </div>
     );
--- src/containers/resources/index.js
+++ src/containers/resources/index.js
@@ -5,13 +5,11 @@ class Resources extends Component {
 
   render() {
     return (
-      <div className="row">
-        <div className="col s12 m9 l10">
-          <div className="card deep-purple lighten-5">
-            <div className="card-content">
-              <h3>Student Resources</h3>
-              <hr />
-            </div>
+      <div className="row resources">
+        <div className="card deep-purple lighten-5">
+          <div className="card-content">
+            <h3>Student Resources</h3>
+            <hr />
           </div>
         </div>
       </div>
