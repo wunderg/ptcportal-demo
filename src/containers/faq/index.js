239aacb1e0d393a065afd753925ff8e3d52ea04c
darwin <oleg.umarov@gmail.com>
2016-04-10 22:06:16 -0500

    setup boilerplate for faq and resources

--- src/containers/faq/index.js
+++ src/containers/faq/index.js
@@ -7,6 +7,12 @@ class Faq extends Component {
     return (
       <div className="row">
         <div className="col s12 m9 l10">
+          <div className="card deep-purple lighten-5">
+            <div className="card-content">
+              <h3>Frequently Asked Questions</h3>
+              <hr />
+            </div>
+          </div>
 
         </div>
       </div>
--- src/containers/resources/index.js
+++ src/containers/resources/index.js
@@ -7,7 +7,12 @@ class Resources extends Component {
     return (
       <div className="row">
         <div className="col s12 m9 l10">
-
+          <div className="card deep-purple lighten-5">
+            <div className="card-content">
+              <h3>Student Resources</h3>
+              <hr />
+            </div>
+          </div>
         </div>
       </div>
     );
