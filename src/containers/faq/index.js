b60114ce01a2ea60a21b4511438b975529310235
darwin <oleg.umarov@gmail.com>
2016-04-10 22:02:35 -0500

    add faq and resource pages

---
+++ src/containers/faq/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+class Faq extends Component {
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col s12 m9 l10">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Faq;
---
+++ src/containers/resources/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+class Resources extends Component {
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col s12 m9 l10">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Resources;
