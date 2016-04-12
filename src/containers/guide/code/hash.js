a3f0bc8924d12be8f60ecb07d185b32ca402146b
darwin <oleg.umarov@gmail.com>
2016-04-11 22:55:01 -0500

    add code for hash

---
+++ src/containers/guide/code/hash.js
@@ -0,0 +1,32 @@
+export const hash =
+`function hash(str){
+  //your code here
+}`;
+
+export const hashSolution =
+`function hash(str) {
+  return reduce(str, function(result, item), {
+    if (!result[item]) {
+      result[item] = 1;
+    } else {
+      result[item] +=1;
+    }
+    return result;
+  }, {});
+}
+
+***** Pseudo Code *****
+// declare function hash
+  // return reduce pass in collection, accumulator and {}
+  //accumulator will accept 2 args result and item
+    //if result has NOT property name as item
+      //create that property and assign value of 1
+    //otherwise
+      //increase item property by one
+  //return result
+`;
+
+export const hashExample =
+`
+hash('abbccc'); => {a: 1, b: 2, c: 3};
+`;
