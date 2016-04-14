f779481760724c41242db3cd78e953dcf1ca913e
darwin <oleg.umarov@gmail.com>
2016-04-14 14:31:52 -0500

    create and add code snippets, closes #28 and #29

---
+++ src/containers/guide/code/mock.js
@@ -0,0 +1,51 @@
+export const array = `var array = [{name: 'Justin'}, 'Justin', 5];`;
+
+export const multiply =
+`function multiply(num) {
+  return num * 2;
+}`;
+
+export const useMult = `multiply(array[2]); => 10`;
+
+export const each =
+`function each(collection, callback){
+  //your code here
+}`;
+
+export const reject =
+`function reject(collection, callback){
+  //your code here
+}`;
+
+export const reduce = `function reduce(collection, accumulator, startValue) {
+  //your code here
+}`;
+
+export const map = `function map(collection, callback) {
+  //your code here
+}`;
+
+export const filterUsingReduce =
+`function = filter(collection predicate())
+  var reduce(collection, predicate(item) {
+    if(!item)
+      result.put(collection);
+    }
+    result = result + 1;
+  }, '')
+}`;
+
+export const numberOfWins =
+  `//example
+numbersOfWins('LWLWLWWWLLWW')  => {L: 5, W: 8};
+
+funciton numberOfWins('str') {
+  //your code here
+}
+`;
+
+export const defaultProps =
+  `// should work as underscore _.default
+function defaultProp(){
+  //your code here
+}`;
--- src/containers/guide/interview.js
+++ src/containers/guide/interview.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import Highlight from '../../helpers/highlight.js';
-
-const each = `function each(collection, callback){
-  //your code here
-}`
-
-const map = `function map(collection, callback) {
-  //your code here
-}`
+import { array, multiply, each, useMult, reject, reduce, map, filterUsingReduce, numberOfWins, defaultProps } from './code/mock.js';
 
 export default () => (
 
@@ -19,47 +12,43 @@ export default () => (
         <div className="card-content">
           <div className="code">
             <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create an array with null, string, and a number</span></h5>
-            <Highlight props={each} />
-          </div>
-          <div className="code">
-            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create an object with 3 properties</span></h5>
-            <Highlight props={each} />
+            <Highlight props={array} />
           </div>
           <div className="code">
             <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create function multiply that multiples number by 2</span></h5>
-            <Highlight props={each} />
+            <Highlight props={multiply} />
           </div>
           <div className="code">
             <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Use multiply to multyply third value in array above</span></h5>
-            <Highlight props={each} />
+            <Highlight props={useMult} />
           </div>
           <div className="code">
             <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create each function that can work on objects, arrays and string</span></h5>
             <Highlight props={each} />
           </div>
           <div className="code">
-            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create filter function using each</span></h5>
-            <Highlight props={each} />
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create reject function using each</span></h5>
+            <Highlight props={reject} />
           </div>
           <div className="code">
             <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create reduce function using each</span></h5>
-            <Highlight props={each} />
+            <Highlight props={reduce} />
           </div>
           <div className="code">
             <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create map using reduce</span></h5>
-            <Highlight props={each} />
+            <Highlight props={map} />
           </div>
           <div className="code">
             <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Debug filter that uses reduce inside</span></h5>
-            <Highlight props={each} />
+            <Highlight props={filterUsingReduce} />
           </div>
           <div className="code">
-            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create numberOfWins funciton</span></h5>
-            <Highlight props={each} />
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create numberOfWins funciton using reduce</span></h5>
+            <Highlight props={numberOfWins} />
           </div>
           <div className="code">
             <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create default function using reduce and each</span></h5>
-            <Highlight props={each} />
+            <Highlight props={defaultProps} />
           </div>
         </div>
       </div>
