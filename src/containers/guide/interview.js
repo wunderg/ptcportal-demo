702024b0b298f56539279ce360f4e3fe6c83b639
darwin <oleg.umarov@gmail.com>
2016-04-14 13:54:43 -0500

    create mock page with prompts, closes #27

--- src/containers/guide/interview.js
+++ src/containers/guide/interview.js
@@ -18,27 +18,47 @@ export default () => (
       <div className="card">
         <div className="card-content">
           <div className="code">
-            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Each should apply the callback to every item in the collection</span></h5>
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create an array with null, string, and a number</span></h5>
             <Highlight props={each} />
           </div>
           <div className="code">
-            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Each should apply the callback to every item in the collection</span></h5>
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create an object with 3 properties</span></h5>
             <Highlight props={each} />
           </div>
           <div className="code">
-            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Each should apply the callback to every item in the collection</span></h5>
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create function multiply that multiples number by 2</span></h5>
             <Highlight props={each} />
           </div>
           <div className="code">
-            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Each should apply the callback to every item in the collection</span></h5>
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Use multiply to multyply third value in array above</span></h5>
             <Highlight props={each} />
           </div>
           <div className="code">
-            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Each should apply the callback to every item in the collection</span></h5>
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create each function that can work on objects, arrays and string</span></h5>
             <Highlight props={each} />
           </div>
           <div className="code">
-            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Each should apply the callback to every item in the collection</span></h5>
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create filter function using each</span></h5>
+            <Highlight props={each} />
+          </div>
+          <div className="code">
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create reduce function using each</span></h5>
+            <Highlight props={each} />
+          </div>
+          <div className="code">
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create map using reduce</span></h5>
+            <Highlight props={each} />
+          </div>
+          <div className="code">
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Debug filter that uses reduce inside</span></h5>
+            <Highlight props={each} />
+          </div>
+          <div className="code">
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create numberOfWins funciton</span></h5>
+            <Highlight props={each} />
+          </div>
+          <div className="code">
+            <h5><i className="fa fa-check green-text text-lighten-2"></i><span> Create default function using reduce and each</span></h5>
             <Highlight props={each} />
           </div>
         </div>
