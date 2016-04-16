da04b7220406e4dae4ac5edd29e78ba75b3a608a
darwin <oleg.umarov@gmail.com>
2016-04-16 12:01:40 -0500

    fix typos lelel 2, closes #4

--- src/containers/guide/level2.js
+++ src/containers/guide/level2.js
@@ -19,9 +19,9 @@ export default () => (
                 <ul className="prompts">
                   <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach what is the predicate function</span></li>
                   <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach the logic behind filter</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach that item is pushed to array only if predicate return true</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach student to walk step by step thru the function using pseudo code</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Practice creating function on the fly</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach that item is pushed to array only if the predicate will return true</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach student how to walk step by step thru the function using pseudo code</span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Help studdent to practice creating functions on the fly</span></li>
                   <li><i className="fa fa-check green-text text-lighten-2"></i><span> Help student to create an example</span></li>
                   <li><i className="fa fa-check green-text text-lighten-2"></i><span> Help student to walk thru every iteration of the function using example</span></li>
                 </ul>
@@ -31,10 +31,10 @@ export default () => (
               </div>
               <ul className="prompts">
                 <li><i className="fa fa-check green-text text-lighten-2"></i><span> Create function filter</span></li>
-                <li><i className="fa fa-check green-text text-lighten-2"></i><span> Filter should use each to iterate</span></li>
+                <li><i className="fa fa-check green-text text-lighten-2"></i><span> Filter should use each to iterate over collection</span></li>
                 <li><i className="fa fa-check green-text text-lighten-2"></i><span> Filter should accept collection and predicate as parameters</span></li>
                 <li><i className="fa fa-check green-text text-lighten-2"></i><span> Filter should work on arrays and objects</span></li>
-                <li><i className="fa fa-check green-text text-lighten-2"></i><span> Filter should return new array with values that pass predicate test</span></li>
+                <li><i className="fa fa-check green-text text-lighten-2"></i><span> Filter should return new array with a values that are passing the predicate test</span></li>
               </ul>
             </div>
             <div className="code">
@@ -62,8 +62,8 @@ export default () => (
                   <li><i className="fa fa-check green-text text-lighten-2 green-text text-lighten-2"></i><span> Teach how to use 'Each' to create Reduce function</span></li>
                   <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach about accumulator and startValue</span></li>
                   <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach that accumulator always takes atleast TWO arguments Memo and Item</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach that startValue will get modified(Memo) by accumulator on every iteration </span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach that startValue is the value that will get returned in the end as the final result </span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach that startValue will be a Memo on the first iteration and it will get modified(Memo) by accumulator on every iteration </span></li>
+                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach that startValue is the value that will be returned in the end as the final result </span></li>
                   <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach that startValue is optional</span></li>
                   <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach that if starting value is not provided Reduce will use first item in the collection as the startValue </span></li>
                   <li><i className="fa fa-check green-text text-lighten-2"></i><span> Teach that if starting value is not provided Reduce should NOT iterate over the first item because it will become the startValue </span></li>
@@ -75,11 +75,11 @@ export default () => (
               <ul className="prompts">
                 <li><i className="fa fa-check green-text text-lighten-2"></i><span> Create function reduce</span></li>
                 <li><i className="fa fa-check green-text text-lighten-2"></i><span> Reduce should apply accumulator to every item in the collection</span></li>
-                <li><i className="fa fa-check green-text text-lighten-2"></i><span> Reduce should pass result of accumulator(MEMO) to every invocation of accumulator</span></li>
+                <li><i className="fa fa-check green-text text-lighten-2"></i><span> Reduce should pass result of accumulator(MEMO, ITEM) to the MEMO on every invocation of accumulator</span></li>
                 <li><i className="fa fa-check green-text text-lighten-2"></i><span> Reduce should use first item in the collection if start is not provided</span></li>
                 <li><i className="fa fa-check green-text text-lighten-2"></i><span> Reduce should NOT invoke accumulator on first item if start in not provided</span></li>
                 <li><i className="fa fa-check green-text text-lighten-2"></i><span> Reduce should return a new array</span></li>
-                <li><i className="fa fa-check green-text text-lighten-2"></i><span> Reduce should use start value as final result</span></li>
+                <li><i className="fa fa-check green-text text-lighten-2"></i><span> Reduce should use startValue as final result</span></li>
               </ul>
             </div>
             <div className="code">
