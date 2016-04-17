c81c0103d9bf274dea3043ff5adc82680256a467
darwin <oleg.umarov@gmail.com>
2016-04-16 23:31:50 -0500

    create login file to store actions for authentication

---
+++ src/actions/login.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import * as ACTIONS from './constants';
+
+
+function requestLogin(creds) {
+  return {
+    type: ACTIONS.LOGIN_REQUEST,
+    isFetching: true,
+    isAuthenticated: false,
+    creds
+  };
+}
+
+
+function recieveLogin(user) {
+  return {
+    type: ACTIONS.LOGIN_SUCESS,
+    isFetching: false,
+    isAuthenticated: true,
+    id_token: user.id_token
+  };
+}
+
+
+function loginError(message) {
+  return {
+    type: ACTIONS.LOGIN_FAILURE,
+    isFetching: false,
+    isAuthenticated: false,
+    message
+  };
+}
+
+export function loginUser(creds, dispatch) {
+  console.log(creds, 'action', dispatch);
+    return dispatch(requestLogin(creds));
+}
