c6c681f845314baf3867cdb7e99c5eefb9a760a4
darwin <oleg.umarov@gmail.com>
2016-04-19 11:54:29 -0500

    change

--- src/helpers/highlight.js
+++ src/helpers/highlight.js
@@ -2,5 +2,6 @@ import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 
 export default ({props}) => {
+
   return <SyntaxHighlighter language='javascript' stylesheet='docco'>{props}</SyntaxHighlighter>;
 }
