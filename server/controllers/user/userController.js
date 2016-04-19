76735c61c9e07dd4f4e777aed9d29addddd3316a
darwin <oleg.umarov@gmail.com>
2016-04-19 18:09:43 -0500

    add signup controller

--- server/controllers/user/userController.js
+++ server/controllers/user/userController.js
@@ -1,3 +1,4 @@
+import User from '../../models/userModel.js';
 
 export default {
   signin(req, res) {
@@ -5,6 +6,35 @@ export default {
     res.json({ id_token: 1234567890 });
   },
 
-  signup(req, res) {
+  signup(req, res, next) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email }, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (user) {
+        return res.status(422).send({ error: 'User already exist' });
+      }
+
+      if (!email || !password) {
+        res.status(422).send({ error: 'Must provide email and password' });
+      }
+
+      var newUser = new User({
+        email,
+        password
+      });
+
+      newUser.save(err2 => {
+        if (err2) {
+          next(err);
+        }
+
+        res.json({ user });
+      });
+    });
   }
 };
