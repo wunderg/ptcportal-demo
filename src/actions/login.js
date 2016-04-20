de0f44d45f4fd817b68b0ae38b9a5677ddcd6554
darwin <oleg.umarov@gmail.com>
2016-04-19 21:25:35 -0500

    create action and reducers for signup

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,6 +1,42 @@
 import axios from 'axios';
 import * as ACTIONS from './constants';
 
+function requestSignup(creds) {
+  return {
+    type: ACTIONS.SIGNUP_REQUEST,
+    isFetching: true,
+    isAuthenticated: false,
+    creds
+  };
+}
+
+function recieveSignup(user) {
+  return {
+    type: ACTIONS.SIGNUP_SUCCESS,
+    isFetching: false,
+    isAuthenticated: true,
+    id_token: user.data.id_token
+  };
+}
+
+
+function signupError(message) {
+  return {
+    type: ACTIONS.SIGNUP_FAILURE,
+    isFetching: false,
+    isAuthenticated: false,
+    message
+  };
+}
+
+export function signupUser(creds, dispatch) {
+  return () => {
+    dispatch(requestSignup(creds));
+    return axios.post('api/signup', creds)
+      .then(res => dispatch(recieveSignup(res)))
+      .catch(err => dispatch(signupError(err)));
+  };
+}
 
 function requestLogin(creds) {
   return {
@@ -10,7 +46,6 @@ function requestLogin(creds) {
     creds
   };
 }
-
 
 function recieveLogin(user) {
   return {
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -7,6 +7,14 @@ export default (state = {}, action) => {
       return {...state, isFetching: true, isAuthenticated: false, user: action.creds};
     case ACTIONS.LOGIN_SUCCESS:
       return {...state, isFetching: false, isAuthenticated: true, id_token: action.id_token};
+    case ACTIONS.LOGIN_FAILURE:
+      return {...state, isFetching: false, isAuthenticated: false, id_token: false};
+    case ACTIONS.SINGUP_REQUEST:
+      return {...state, isFetching: true, isAuthenticated: false, user: action.creds};
+    case ACTIONS.SINGUP_SUCCESS:
+      return {...state, isFetching: false, isAuthenticated: true, id_token: action.id_token};
+    case ACTIONS.SINGUP_FAILURE:
+      return {...state, isFetching: false, isAuthenticated: false, id_token: false};
     default:
       return state;
   }
