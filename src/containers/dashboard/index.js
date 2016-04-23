05f2dc50aac663742d1cc6be52abbe8c32e66bd1
darwin <oleg.umarov@gmail.com>
2016-04-23 15:00:52 -0500

    replace home with dashboard component, close #52

---
+++ src/containers/dashboard/index.js
@@ -0,0 +1,52 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchStudents } from '../../actions';
+
+import TextInput from '../text-input';
+import Students from '../students';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.props.fetchStudents();
+  }
+
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col s12">
+          <TextInput />
+        </div>
+        <div className="col s12">
+          <Students data={ this.props.data } />
+        </div>
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {
+};
+
+function mapStateToProps(state) {
+  return {
+    data: state.slocal
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchStudents }, dispatch);
+}
+
+Home.propTypes = {
+  data: PropTypes.object,
+  fetchStudents: PropTypes.func,
+  history: PropTypes.object
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
