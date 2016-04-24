fbdcca8f3ef66c283d58c6d7e3888d074c6082ed
darwin <oleg.umarov@gmail.com>
2016-04-24 17:45:04 -0500

    redesign and add validation to signup page, close #73

--- src/containers/login/signup.js
+++ src/containers/login/signup.js
@@ -3,10 +3,9 @@ import { reduxForm } from 'redux-form';
 import './style.scss';
 import { loginUser, signupUser } from '../../actions/login.js';
 
-class Login extends Component {
+class Signup extends Component {
   constructor(props) {
     super(props);
-    this.onSignup = this.onSignup.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
@@ -19,54 +18,46 @@ class Login extends Component {
   componentWillReceiveProps(props) {
     if (props.user.isAuthenticated) {
       this.context.router.push('/dashboard');
-      this.props.resetForm();
     }
   }
 
-  onSubmit(e) {
+  onSubmit(e, z) {
     e.preventDefault();
     const creds = {
-      email: this.props.data.email.value,
-      pass: this.props.data.pass.value
-
-    };
-
-    this.props.loginUser(creds);
-  }
-
-  onSignup(e) {
-    e.preventDefault();
-    const creds = {
-      email: this.props.data.email.value,
-      pass: this.props.data.pass.value
-
+      email: this.props.data.email.value || null,
+      pass: this.props.data.pass.value || null
     };
 
     this.props.signupUser(creds);
   }
 
   render() {
-    const { fields: { email, pass }, user } = this.props;
+    const { fields: { email, pass, pass2 }, user, resetForm, submitting } = this.props;
     return (
         <div className="middle valign-wrapper">
           <form className="login-form" onSubmit={this.onSubmit}>
-            <div className="error-message center">{user.message}</div>
+            <div className="error-message center">{user.message}
+              {email.touched && email.error && <div>{email.error}</div>}
+              {pass.touched && pass.error && <div>{pass.error}</div>}
+            </div>
             <div className="input-field">
               <input type="text" className="validate form-control" {...email} />
-              <label htmlFor="icon_prefix">Email</label>
+              <label>Email</label>
             </div>
             <div className="input-field">
               <input type="password" className="validate form-control" {...pass} />
-              <label htmlFor="icon_telephone">Password</label>
+              <label>Password</label>
+            </div>
+            <div className="input-field">
+              <input type="password" className="validate form-control" {...pass2} />
+              <label>Confirm Password</label>
             </div>
             <div className="center-align login-buttons">
-            <button onClick={this.onSignup} className="btn waves-effect waves-light">
-              SignUP
-              <i className="fa fa-info fa-2x fa-spin right"></i>
+            <button onClick={resetForm} disabled={submitting} className="btn waves-effect waves-light">
+              Clear
             </button>
-            <button onClcik={this.onLogin} className="btn waves-effect waves-light">
-              Login
-              <i className="fa fa-info fa-2x fa-spin right"></i>
+            <button onClick={this.onSubmit} disabled={email.error || pass.error} className="btn waves-effect waves-light">
+              Signup
             </button>
             </div>
           </form>
@@ -75,11 +66,11 @@ class Login extends Component {
   }
 }
 
-Login.contextTypes = {
+Signup.contextTypes = {
   router: PropTypes.object.isRequired
 };
 
-Login.propTypes = {
+Signup.propTypes = {
   fields: PropTypes.object,
   data: PropTypes.object,
   resetForm: PropTypes.func,
@@ -90,14 +81,35 @@ Login.propTypes = {
   user: PropTypes.object,
 };
 
+const validate = values => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = 'Email Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  if (!values.pass) {
+    errors.pass = 'Password Required';
+  } else if (values.pass.length < 6) {
+    errors.pass = 'Has to be atleast 6 characters';
+  } else if (values.pass !== values.pass2) {
+    errors.pass = 'Passwords should MATCH';
+  }
+
+  return errors;
+}
+
 function mapStateToProps(state) {
   return {
-    data: state.form.login,
+    data: state.form.signup,
     user: state.user
   };
 }
 
 export default reduxForm({
-  form: 'login',
-  fields: ['email', 'pass']
-}, mapStateToProps, { loginUser, signupUser })(Login);
+  form: 'signup',
+  fields: ['email', 'pass', 'pass2'],
+  validate
+}, mapStateToProps, { loginUser, signupUser })(Signup);
