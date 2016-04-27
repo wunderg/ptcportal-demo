73de95bd289a8be96eed155117ea1ff6acc3f8fa
darwin <oleg.umarov@gmail.com>
2016-04-26 23:52:02 -0500

    make first test to pass

--- test/actions/index.js
+++ test/actions/index.js
@@ -5,8 +5,18 @@ import { addStudent } from '../../src/actions/index.js';
 
 describe('actions', () => {
   describe('add student', () => {
+    let data = {
+      _id: 5,
+      lesson: 0,
+      level: 0,
+      interview: 'not set',
+      decision: 'pending',
+      name: 'John',
+      email: 'Josh@JOSH.com'
+    };
+
     it('has to be correct type', () => {
-      const action = addStudent();
+      const action = addStudent(data);
       expect(action.type).to.equal(ACTIONS.ADD_STUDENT);
     });
   });
--- test/test_helper.js
+++ test/test_helper.js
@@ -5,6 +5,28 @@ import TestUtils from 'react-addons-test-utils';
 import ReactDom from 'react-dom';
 import chai, { expect } from 'chai';
 import { Provider } from 'react-redux';
-import { createStore} from 'redux';
+import { createStore } from 'redux';
 import { rootReducer } from '../src/store/index.js';
 import chaiJquery from 'chai-jquery';
+
+
+export { expect };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
