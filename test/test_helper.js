50e978e8d2ef543d9937b8037bf906ee29cfbb86
darwin <oleg.umarov@gmail.com>
2016-04-27 22:49:14 -0500

    complete test helper file

--- test/test_helper.js
+++ test/test_helper.js
@@ -10,23 +10,30 @@ import { rootReducer } from '../src/store/index.js';
 import chaiJquery from 'chai-jquery';
 
 
-export { expect };
+// setup testing enviroment like browser in command line
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+const $ = jquery(global.window);
 
+// build render component that will render component for us
+function renderComponent(ComponentClass, props, state) {
+  const componentInstance = TestUtils.renderIntoDocument(
+    <Provider store={createStore(rootReducer, state)}>
+      <ComponentClass {...props} />
+    </Provider>
+  );
+  return $(ReactDom.findDOMNode(componentInstance)); // => creates html
+}
 
+$.fn.simulate = function (eventName, value) {
+  if (value) {
+    this.val(value);
+  }
 
+  TestUtils.Simulate[eventName](this[0]);
+};
 
+// setup chai jquery
+chaiJquery(chai, chai.util, $);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export { renderComponent, expect };
