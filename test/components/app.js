3ef7866e68099dafd5f062478d98ce0d3d6b154a
darwin <oleg.umarov@gmail.com>
2016-04-29 11:23:35 -0500

    testing in progress

--- test/components/app.js
+++ test/components/app.js
@@ -1,16 +1,18 @@
-import { renderComponent, expect } from '../test_helper.js';
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
 import App from '../../src/containers/app/index.js';
+
 
 describe('App', () => {
 
   it('shows main component ', () => {
-    const component = renderComponent(App);
-    console.log(component.find('main-wrap'));
-    expect(component.find('main-wrap')).to.have.class('main-wrap');
+    const component = mount(<App />)
+    console.log(component);
     {/* expect(component).to.have.class('main-wrap'); */}
   });
 
-  it('shows navbar component', () => {
+  xit('shows navbar component', () => {
     const component = renderComponent(App, null, {});
     expect(component.find('.nav-wrapper')).to.exist;
   });
