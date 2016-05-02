89637dfd94bbd37998d83da8a84587478b1032c2
Ben <benjaminjbaum@gmail.com>
2016-05-02 13:09:27 -0500

    Fix/sum solution (#92)
    * (fix) fix typo
    
    * (fix) correct solution and pseudocode for sum using reduce

--- src/containers/guide/code/sum.js
+++ src/containers/guide/code/sum.js
@@ -14,18 +14,14 @@ sum(numbers, 10); => 20
 
 export const sumSolution =
 `function sum(collection, startValue) {
-  startValue = startValue || 0;
   return reduce(collection, function(memo, item) {
-    memo + item;
-    return memo;
-  }, startValue);
+    return memo + item;
+  });
 }
 
 ***** Pseudo Code *****
 //function sum accepts collection and optional startValue
-  //if startValue is not defined initiate at 0
   //return reduce with collection, callback and startVallue
     //pass memo and item into the callback
-    //add memo and item
-    //return memo
+    //add memo and item and return result
 `;
