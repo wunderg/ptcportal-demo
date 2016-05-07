6a71a03e3736a462bcf77545eb46782979eb1ad0
darwin <oleg.umarov@gmail.com>
2016-05-07 16:50:41 -0500

    add ability to update student rating, close #125

--- src/containers/student/ratings.js
+++ src/containers/student/ratings.js
@@ -3,12 +3,14 @@ import StarRating from 'react-star-rating';
 import './style.scss';
 
 export default (props) => {
-  const updateRating = () => {
-    props.updateStudent(props.data);
+  const updateRating = (_, stars) => {
+    console.log('updateRating');
+    const student = props.data;
+    student.ratings[stars.name] = stars.rating;
+    props.updateStudent(student);
   };
   return (
     <div className="card">
-      {console.log(props.data.ratings.logic, 'STARS')}
       <div className="card-content black-text">
         <span className="card-title">Ratings</span>
         <hr />
@@ -16,31 +18,73 @@ export default (props) => {
           <span>Logic</span>
           <StarRating
             className="star"
-            size={30} name="airbnb-rating"
+            size={30}
+            name="logic"
             totalStars={7}
             rating={props.data.ratings.logic}
             onRatingClick={updateRating}
+            editing={true}
           />
         </div>
         <div className="stars">
           <span>Motivation</span>
-          <StarRating className="star" size={30} name="airbnb-rating" totalStars={7} />
+          <StarRating
+            className="star"
+            size={30}
+            totalStars={7}
+            name="motiv"
+            rating={props.data.ratings.motiv}
+            onRatingClick={updateRating}
+            editing={true}
+          />
         </div>
         <div className="stars">
           <span>Effort</span>
-          <StarRating className="star" size={30} name="airbnb-rating" totalStars={7} />
+          <StarRating
+            className="star"
+            size={30}
+            totalStars={7}
+            name="effort"
+            rating={props.data.ratings.effort}
+            onRatingClick={updateRating}
+            editing={true}
+          />
         </div>
         <div className="stars">
           <span>Likebility</span>
-          <StarRating className="star" size={30} name="airbnb-rating" totalStars={7} />
+          <StarRating
+            className="star"
+            size={30}
+            totalStars={7}
+            name="ethic"
+            rating={props.data.ratings.ethic}
+            onRatingClick={updateRating}
+            editing={true}
+          />
         </div>
         <div className="stars">
           <span>Explanation</span>
-          <StarRating className="star" size={30} name="airbnb-rating" totalStars={7} />
+          <StarRating
+            className="star"
+            size={30}
+            totalStars={7}
+            name="explain"
+            rating={props.data.ratings.explain}
+            onRatingClick={updateRating}
+            editing={true}
+          />
         </div>
         <div className="stars">
           <span>Confidence</span>
-          <StarRating className="star" size={30} name="airbnb-rating" totalStars={7} />
+          <StarRating
+            className="star"
+            size={30}
+            totalStars={7}
+            name="confid"
+            rating={props.data.ratings.confid}
+            onRatingClick={updateRating}
+            editing={true}
+          />
         </div>
       </div>
     </div>
