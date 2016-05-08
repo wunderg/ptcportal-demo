443b336c4dfb19a99215a239b3e1223f2a4e12bd
darwin <oleg.umarov@gmail.com>
2016-05-07 23:21:25 -0500

    create session template from notes

--- src/containers/student/sessions.js
+++ src/containers/student/sessions.js
@@ -1,33 +1,75 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import Highlight from '../../helpers/highlight.js';
 
-var str = "function each(a, b) \n {for(var i = 0; i < a.length; i++) { \n b(a[i]); }}";
-export default (props) => (
-  <div className="card">
-    <div className="card-content black-text">
-      <span className="card-title">
-        Sessions
-        <button className="btn-floating btn-large waves-effect waves-light blue right"><i className="fa fa-plus"></i></button>
-      </span>
-      <hr />
-      <ul className="collapsible" data-collapsible="expandable">
-        <li>
-          <div className="collapsible-header center">First</div>
-          <div className="collapsible-body">
-            <Highlight props={str} />
-                {/* {str.split(" {").map(item => <Highlight props={item} />)} */}
-          </div>
-        </li>
-        <li>
-          <div className="collapsible-header center">Second</div>
-          <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-        </li>
-        <li>
-          <div className="collapsible-header center">Third</div>
-          <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
+class Sessions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.updateSessions = this.updateSessions.bind(this);
+  }
 
+  handleTextChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  updateSessions() {
+    if (this.state.text) {
+      const student = this.props.data;
+      student.notes.push(this.state.text);
+      this.props.updateStudent(student);
+      this.forceUpdate();
+    }
+  }
+
+  render() {
+    const rows = this.props.data.notes.map((note, index) => (
+      <li key={index}>
+        <div className="collapsible-header center">Session: {index}</div>
+        <div className="collapsible-body"><p>{note}</p></div>
+      </li>
+      )
+    );
+
+    return (
+      <div className="card">
+        <div className="card-content black-text">
+          <span className="card-title">
+            Sessions
+            <button href="#modal1" className="modal-trigger btn-floating btn-large waves-effect waves-light blue right">
+              <i className="fa fa-plus"></i>
+            </button>
+          </span>
+          <hr />
+          <ul className="collapsible" data-collapsible="expandable">
+            { rows }
+          </ul>
+        </div>
+        <div id="modal1" className="modal">
+          <div className="modal-content">
+            <h4>Sessions</h4>
+            <p>What do you think of the student performance today?</p>
+            <textarea
+              value={this.state.text}
+              onChange={this.handleTextChange}
+              style={{ height: 250 }}
+            />
+          </div>
+          <div className="modal-footer">
+            <a onClick={this.updateSessions} className=" modal-action modal-close waves-effect waves-blue btn-flat btn">Save</a>
+            <a className=" modal-action modal-close waves-effect waves-blue btn-flat btn">Close</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Sessions.propTypes = {
+  data: PropTypes.object,
+  updateStudent: PropTypes.func
+};
+
+export default Sessions;
