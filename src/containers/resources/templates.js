30b3233af62dcfd670422e6866f68817a7f4bf71
darwin <oleg.umarov@gmail.com>
2016-05-19 18:35:38 -0500

    update templates with new info, closes #9

--- src/containers/resources/templates.js
+++ src/containers/resources/templates.js
@@ -9,20 +9,34 @@ export const Initial = () => (
 
       <br />
       <br />
-    Let me know what is best time for you this week. I have slot  at [ **6.30pm–7–30pm CST this Wednesday or Tuesday** ]. For your reference, we’ll meet for 45–60 minutes twice a week.
+      You can view my calendar and book times for us to meet twice a week using this link.[LINK HERE]. For your reference, we’ll meet for 45–60 minutes twice a week. We are going to use Skype for our sessions and I will contact you at time of your appoitment.
       <br />
       <br />
+    Here's a bit more detail about the program and expectations we have have to help you unlock your skills and achieve your goals. You should be ready to commit to the following:
+      <br />
+      <br />
+    1. Be prepared to put in at least 1-2 hours of practice per day doing toy problems and code challenges, practicing programming by building out a pet project, etc. This purposefully excludes watching lectures on YouTube, etc. because we want you to experience the doing of programming.
+      <br />
+    2. Bring questions to class each time we meet so you can demonstrate what you're working on outside of class and get more out of our sessions. Ultimately, this will help you to advance more quickly.
+      <br />
+    3. Treat this program and your time in it with respect. Show up on time having prepped and be ready to put in strong effort to learn and advance.
 
+      <br />
+      <br />
+    If you don't agree to the above, please contact remote.beta.admissions@hackreactor.com to discuss more.
+      <br />
+      <br />
 
     A little bit about me and what I do here: as an HiR I've been [on an engineering team, teach remote prep classes, do this coaching, help with outcomes, run a help desk, do technical interviews, etc]; I've been into tech/programming since [1967 but had been away from it for a few years - some tech background]; I like [jazz, penguins, and travel - some non-tech background].
 
      <br />
      <br />
-    Reply to me here ( [first.last]@hackreactor.com ) and let me know what time works for you this week - [restate_someTime], and if you've got any other questions or scheduling concerns.
+     Using my calendar link above please book sessions (2x/week) for the coming week. Then, reply to me here ( [first.last]@hackreactor.com ) if you've got any questions or scheduling concerns.
+
 
     <br/>
     <br/>
-            Thanks!
+Thank you and I'm looking forward to working with you!
 
    </p>
         </section>
