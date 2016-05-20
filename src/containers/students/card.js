121b23569623be3e1bc7a8bfe1bd8a8b755528bc
darwin <oleg.umarov@gmail.com>
2016-05-20 01:42:06 -0500

    fix email typos

--- src/containers/students/card.js
+++ src/containers/students/card.js
@@ -12,9 +12,10 @@ You can view my calendar and book times for us to meet twice a week using this l
 Here's a bit more detail about the program and expectations we have have to help you unlock your skills and achieve your goals. You should be ready to commit to the following:
 %0D%0A%0D%0A
 1. Be prepared to put in at least 1-2 hours of practice per day doing toy problems and code challenges, practicing programming by building out a pet project, etc. This purposefully excludes watching lectures on YouTube, etc. because we want you to experience the doing of programming.
-2. Bring questions to class each time we meet so you can demonstrate what you're working on outside of class and get more out of our sessions. Ultimately, this will help you to advance more quickly.
-3. Treat this program and your time in it with respect. Show up on time having prepped and be ready to put in strong effort to learn and advance.
 %0D%0A%0D%0A
+2. Bring questions to class each time we meet so you can demonstrate what you're working on outside of class and get more out of our sessions. Ultimately, this will help you to advance more quickly.
+%0D%0A%0D%0A
+3. Treat this program and your time in it with respect. Show up on time having prepped and be ready to put in strong effort to learn and advance.
 If you don't agree to the above, please contact remote.beta.admissions@hackreactor.com to discuss more.
 %0D%0A%0D%0A
 A little bit about me and what I do here: as an HiR I've been [on an engineering team, teach remote prep classes, do this coaching, help with outcomes, run a help desk, do technical interviews, etc]; I've been into tech/programming since [1967 but had been away from it for a few years - some tech background]; I like [jazz, penguins, and travel - some non-tech background].
