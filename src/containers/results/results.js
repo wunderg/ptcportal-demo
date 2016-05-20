a983c943241f72737cf39f79d75a611ebff1e736
darwin <oleg.umarov@gmail.com>
2016-05-19 20:54:06 -0500

    initial layout for result page

--- src/containers/results/results.js
+++ src/containers/results/results.js
@@ -1,11 +1,36 @@
 import React, { Component, PropTypes } from 'react';
+import './style.scss';
+
+import Row from './row.js';
 
 
 export class Results extends Component {
   render() {
     return (
-      <div className="">
-        Results will be here
+      <div className="row">
+        <div className="col s12">
+
+          <table className="highlight results-table">
+            <thead>
+              <tr>
+                <th data-field="id">Instructor Name</th>
+                <th data-field="name">Students Now</th>
+                <th data-field="price">Students Accepted</th>
+                <th data-field="price">Will complete in two weeks</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <Row />
+              <tr>
+                <td>Total</td>
+                <td>5</td>
+                <td>10</td>
+                <td>20</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
---
+++ src/containers/results/row.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+    <tr>
+      <td>Alvin</td>
+      <td>3</td>
+      <td>5</td>
+      <td>23</td>
+    </tr>
+);
