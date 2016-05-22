1d84fb7608aa7e3f4c911cd1ad75ca213aa5f18f
darwin <oleg.umarov@gmail.com>
2016-05-22 18:28:10 -0500

    delete workflow

--- src/containers/guide/howto.js
+++ src/containers/guide/howto.js
@@ -17,46 +17,6 @@ export default () => (
                     <i className="fa fa-check green-text text-lighten-2"></i>
                     <span> Student has to start at Level 1</span>
                   </li>
-                  <li>
-                    <i className="fa fa-check green-text text-lighten-2"></i>
-                    <span> Every session must start with Level 1 and move up levels from there.</span>
-                  </li>
-                  <li>
-                    <i className="fa fa-check green-text text-lighten-2"></i>
-                    <span> If a level has been completed before, only one lesson should be chosen from that level in proceeding sessions.</span>
-                  </li>
-                  <li>
-                    <i className="fa fa-check green-text text-lighten-2"></i>
-                    <span> For example, in the third session, a student would complete each from lesson 1, reduce from lesson 2 and both sum and getMax from lesson 3.</span>
-                  </li>
-                  <li>
-                    <i className="fa fa-check green-text text-lighten-2"></i>
-                    <span> Student has to write every function from scratch line by line without referencing to any outside material.</span>
-                  </li>
-                  <li>
-                    <i className="fa fa-check green-text text-lighten-2"></i>
-                    <span> Student has to explain every solution on a high-level and aslo line by line. </span>
-                  </li>
-                  <li>
-                    <i className="fa fa-check green-text text-lighten-2"></i>
-                    <span> Never copy and paste code from this guide to student. </span>
-                  </li>
-                  <li>
-                    <i className="fa fa-check green-text text-lighten-2"></i>
-                    <span> Use Pseudo-code to show student how you would talk about the function. </span>
-                  </li>
-                  <li>
-                    <i className="fa fa-check green-text text-lighten-2"></i>
-                    <span> Before Mock Interview, student should be able to complete level 1-7 within ONE session timeframe. </span>
-                  </li>
-                  <li>
-                    <i className="fa fa-check green-text text-lighten-2"></i>
-                    <span> Note: If a student is not comfortable in writing functions without examples, have them reference their own implementations. </span>
-                  </li>
-                  <li>
-                    <i className="fa fa-check green-text text-lighten-2"></i>
-                    <span>If you have concerns about the student in terms of red flags such as memorization or inability to actually reason and logic and think like an engineer, you should address these concerns to the Admissions Manager<a href="josh.shaman@hackreactor.com">josh.shaman@hackreactor.com</a> prior to having the student schedule an interview.</span>
-                  </li>
                 </ul>
               </div>
             </div>
@@ -74,27 +34,6 @@ export default () => (
                     <i className="fa fa-check green-text text-lighten-2"></i>
                     <span> Never give up the ANSWER to the student</span>
                   </li>
-
-                  <li><i className="fa fa-check green-text text-lighten-2"></i>
-                    <span> Be kind and courteous. Being mean or rude scares people off and makes people not want to participate.</span></li>
-
-                  <li><i className="fa fa-check green-text text-lighten-2"></i>
-                    <span> Low-effort criticism is for the benefit of the person delivering it, not the person receiving it.</span></li>
-
-                  <li>
-                    <i className="fa fa-check green-text text-lighten-2"></i>
-                    <span> No feigning surprise. Don't act surprised when somebody says they don't know something. They'll feel terrible and you've accomplished nothing except to make yourself feel sharp.</span>
-                  </li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> No well-actually's. When someone says something that's almost - but not entirely - correct, and you say, "well, actually…" and then give a minor correction. This is especially annoying when the correction has no bearing on the actual conversation. </span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Walking thru every iteration is the best way learn</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Help student to develop 'Code Compiler' in his/her head</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Let student to write his solution before the suggestions</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Lead a student to correct answer with suggestions and small hints</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Be encouraging, polite and supportive thru every step</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Make sure to PRAISE STUDENT on every lesson completed</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Even simple 'Good Job' is sufficent</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Show student how to use chrome debbuger to walk thru every iteration</span></li>
-                  <li><i className="fa fa-check green-text text-lighten-2"></i><span> Every session push them a little further</span></li>
                 </ul>
               </div>
             </div>
